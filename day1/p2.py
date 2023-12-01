5c6f95b0c70b1024def306ce294a09efbfaa4eb6
CAG2Mark <markng989@gmail.com>
2023-12-01 23:51:55 +0800

    improve code

--- day1/p2.py
+++ day1/p2.py
@@ -1,18 +1,18 @@
 from typing import List
 
-digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 def solve(inp: List[str]):
     sm = 0
     for ln in inp:
         cur = ''
         for i in range(len(ln)):
             chr = ln[i]
-            if ord(chr) < ord('0') or ord(chr) > ord('9'):
-                for (j, d) in enumerate(digits):
-                    if ln[i:].startswith(d):
-                        cur += str(j)
-            else:
+            if chr.isdigit():
                 cur += chr
+            else:
+                for (j, d) in enumerate(digits):
+                    if ln.startswith(d, i):
+                        cur += str(j + 1)
         if cur:
             x = cur[0] + cur[-1]
             sm += int(x)
