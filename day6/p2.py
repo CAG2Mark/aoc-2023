288a78c21a2869f8b90a361cc261110420c571f0
CAG2Mark <markng989@gmail.com>
2023-12-06 16:29:49 +0800

    Account for case where roots are an integer

--- day6/p2.py
+++ day6/p2.py
@@ -1,6 +1,9 @@
 from typing import List
 import math
 
+def near_integer(x: float):
+    return abs(x - int(x)) < 1e-12
+
 def solve(inp: List[str]):
     times = [x for x in inp[0].split()[1:]]
     dists = [x for x in inp[1].split()[1:]]
@@ -35,5 +38,12 @@ def solve(inp: List[str]):
 
     lower = 0.5 * (time - math.sqrt(time*time - 4*record))
     upper = 0.5 * (time + math.sqrt(time*time - 4*record))
+
+    d = 0 
+    if near_integer(lower):
+        d -= 1
+    if near_integer(upper):
+        d -= 1
+
         
-    return math.floor(upper) - math.ceil(lower) + 1
+    return math.floor(upper) - math.ceil(lower) + 1 + d
